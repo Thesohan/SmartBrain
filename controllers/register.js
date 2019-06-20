17d6ef7bcc6aec25d51d70f01e7b91070547ae19
sohan <sohan.cse16@nituk.ac.in>
2019-06-20 07:34:10 +0530

    register modified

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 
 const handleRegister = (req,res,db,bcrypt) => {
      const {email,name,password} = req.body;
-
+console.log(email,name,password,"dfg");
      if(!email || !name || !password){
        return res.status(400).json("Wrong form submission")
      }
@@ -9,36 +9,47 @@ const handleRegister = (req,res,db,bcrypt) => {
      //storing the password
      var saltRound=10;
      var hash = bcrypt.hashSync(password,saltRound);
-     // console.log(hash,"hasdsfasd");
+     console.log(hash,"hasdsfasd");
+
+// db('users')
+// .returning('*')
+// .insert({
+//   email:email,
+//   name:name,
+//   joined: new Date()
+// })
+// .then(user=>{
+//   res.json(user[0]);
+// })
+// .catch(err=>res.status(400).json('unaasd'))
+// };
+//
 
      //WE USE transaction to keep consistency within the app
+
      db.transaction(trx => {
-       trx('login').transacting(trx).insert({
+       // console.log(trx);
+       trx.insert({
          hash: hash,
          email: email
        })
-       .then(response =>{
+       .into('login')
+       .returning('email')
+       .then(loginEmail =>{
+         console.log(loginEmail);
          return trx('users')
-           // .returning('*')//it tells that return all columns of the row , not supprted in mysql
+           .returning('*')//it tells that return all columns of the row , not supprted in mysql
            .insert({
              name:name,
-             email:email,
+             email:loginEmail[0],
              joined: new Date()
            })
            //response will be having id
-           .then(response =>{
-             console.log(response);
-             return  trx.select('*').from('users').where({id:response[0]})
-                 .then(data=>{
-                     // console.log(data[0]);
-                   //sending response back
-                   res.json(data[0]);
-                 })
-                 .catch(err => res.status(404).json(err));
-             // res.json(db('users').where({id:response}).select('*'))
-             // res.json(response)
-           }).catch(err=> res.status(404).json("something went wrong"));
-         // .then(console.log);
+           .then(user =>{
+             // console.log("userse",user);
+             res.json(user);
+           })
+           .catch(err=> res.status(404).json("something went wrong"));
            // whenever you do a post request make sure to return a response
            // res.json(database.users[database.users.length-1]);
          })
